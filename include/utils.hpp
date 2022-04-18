0856521612c1f54f53901701e4f60ebef6f60d18
Hugo Santos <hf.santos2332@gmail.com>
2022-04-17 21:24:50 -0300

    Cria funcoes utilitarias para auxiliar no projeto

---
+++ include/utils.hpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+/**
+ * Divide uma string com base em um caractere separador.
+ * 
+ * @param texto string que será dividida 
+ * @param separador caractere que será considerado para separar as partes da string
+ * @return Um std::vector de strings com as partes da string separada 
+ */
+std::vector<std::string> dividirString(std::string &texto, char separador);
+
+/**
+ * @brief Verifica se uma string possui algum caractere que não está entre a e z (Maiúsculo ou minúsculo).
+ * 
+ * @param texto String a ser verificada
+ * @return true se a string possui algum caractere que não esteja entre a e z (Ou A e Z) e false caso contrário.
+ */
+bool tem_caractere_especial(std::string &texto);
+
+/**
+ * @brief Verifica se uma determinada string pode ser convertida em inteiro.
+ * 
+ * @param texto String a ser verificada
+ * @return true se a string puder ser convertida em inteiro e false caso contrário.
+ */
+bool pode_ser_int(std::string texto);
---
+++ src/utils.cpp
@@ -0,0 +1,40 @@
+#include "utils.hpp"
+
+std::vector<std::string> dividirString(std::string &texto, char separador){
+    std::vector<std::string> partes = {};
+    size_t comeco = 0;
+    size_t fim = texto.find(separador, comeco);
+
+    while(fim!= std::string::npos){ // Enquanto encontrar o caractere separador na string.
+        partes.push_back(texto.substr(comeco, fim-comeco));
+        comeco = fim+1;
+
+        fim = texto.find(separador, comeco);
+    }
+
+    return partes;
+}
+
+bool tem_caractere_especial(std::string &texto){
+    for(size_t i=0;i<texto.size();i++){
+        const bool entre_a_z = texto[i]>='a' and texto[i]<='z';
+        const bool entre_A_Z = texto[i]>='A' and texto[i]<='Z';
+
+        if(!(entre_a_z or entre_A_Z))
+            return true;
+    }
+
+    return false;
+}
+
+bool pode_ser_int(std::string texto){
+    try{
+        int number = std::stoi(texto);
+
+        return true;
+    }catch(std::invalid_argument const &e){
+        return false;
+    }
+
+    return false;
+}
